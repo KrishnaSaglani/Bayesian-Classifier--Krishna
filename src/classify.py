ed1969f4067f2283390707badf7e6558637032c0
Krishna Saglani <b23cs1027@iitj.ac.in>
2025-04-04 23:59:18 +0530

    little bit more work on classify.py This i had done earlier, just incorporated in now

--- src/classify.py
+++ src/classify.py
@@ -6,3 +6,24 @@ from skimage.feature import local_binary_pattern
 # Load trained model parameters
 with open('fruits-360/model_params.pkl', 'rb') as f:
     model_params = pickle.load(f)
+
+class_priors = model_params['class_priors']
+means = model_params['means']
+variances = model_params['variances']
+unique_classes = model_params['unique_classes']
+
+
+def gaussian_likelihood(x, mean, var):
+    exponent = np.exp(-((x - mean) ** 2) / (2 * var))
+    return (1 / np.sqrt(2 * np.pi * var)) * exponent
+
+def classify_image(image_path):
+    x = extract_features_from_image(image_path)
+    class_probs = {}
+
+    for c in unique_classes:
+        prior = np.log(class_priors[c] + 1e-9)
+        likelihood = np.sum(np.log(gaussian_likelihood(x, means[c], variances[c]) + 1e-9))
+        class_probs[c] = prior + likelihood  # Log posterior
+
+    return max(class_probs, key=class_probs.get)
